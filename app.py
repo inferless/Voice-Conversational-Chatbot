041e3e07cf033b7c469e6dab79ac676bbaf816ec
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-03-23 03:33:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,67 +1,73 @@
 from faster_whisper import WhisperModel
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from bark import SAMPLE_RATE, generate_audio, preload_models
-import time
 import numpy as np
 import io
 import base64
 import soundfile as sf
 import nltk
 
-class InferlessPythonModel:  
+class InferlessPythonModel:
     def initialize(self):
-        # load speech to text model
+        # Load speech to text model
+        self.audio_file = "output.mp3"
         model_size = "large-v3"
         self.model_whisper = WhisperModel(model_size, device="cuda", compute_type="float16")
         
         # Load Mistral instruct, text to text model
-        model_id = "mistralai/Mistral-7B-Instruct-v0.2"  # Specify the model repository ID
-        # Define sampling parameters for model generation
-        # self.sampling_params = SamplingParams(temperature=0.7, top_p=0.95, max_tokens=128)
-        # Initialize the LLM object
-        # self.llm = LLM(model=model_id)
+        model_id = "mistralai/Mistral-7B-Instruct-v0.2"
         self.model_mistral = AutoModelForCausalLM.from_pretrained(model_id).to("cuda")
         self.tokenizer = AutoTokenizer.from_pretrained(model_id)
         
         # Load Bark, Text to Speech
-        nltk.download('punkt')
+        self.SPEAKER = "v2/en_speaker_6"
         preload_models()
-    
+
+        # Download nltk punkt
+        nltk.download('punkt')
+       
     def base64_to_mp3(self, base64_data, output_file_path):
+        # Convert base64 audio data to mp3 file
         mp3_data = base64.b64decode(base64_data)
         with open(output_file_path, "wb") as mp3_file:
-                mp3_file.write(mp3_data)
+            mp3_file.write(mp3_data)
 
     def infer(self, inputs):
         audio_data = inputs["audio_base64"]
-        audio_file = "output.mp3"
-        self.base64_to_mp3(audio_data,audio_file)
-        segments, info = self.model_whisper.transcribe(audio_file, beam_size=5)
+        self.base64_to_mp3(audio_data, self.audio_file)
+        
+        # Transcribe audio to text
+        segments, info = self.model_whisper.transcribe(self.audio_file, beam_size=5)
         user_text = ''.join([segment.text for segment in segments])
 
+        # Generate prompt for Mistral model
         messages = [{"role": "user", "content": f"You are a helpful, respectful and honest assistant. Answer the following question in exactly in few words from the context. {user_text}"}]
-        encodeds = self.tokenizer.apply_chat_template(messages, return_tensors="pt",add_generation_prompt=True)
+        encodeds = self.tokenizer.apply_chat_template(messages, return_tensors="pt", add_generation_prompt=True)
         model_inputs = encodeds.to("cuda")
         
-        generated_ids = self.model_mistral.generate(model_inputs, max_new_tokens=80, do_sample=True,)
-        # decoded = tokenizer.batch_decode(generated_ids)
-        generated_text = self.tokenizer.batch_decode(generated_ids[:, encodeds.shape[1]:],skip_special_tokens=True)[0]
+        # Generate text response using Mistral model
+        generated_ids = self.model_mistral.generate(model_inputs, max_new_tokens=80, do_sample=True)
+        generated_text = self.tokenizer.batch_decode(generated_ids[:, encodeds.shape[1]:], skip_special_tokens=True)[0]
 
+        # Process generated text into audio
         script = generated_text.replace("\n", " ").strip()
         sentences = nltk.sent_tokenize(script)
-        SPEAKER = "v2/en_speaker_6"
+        
         silence = np.zeros(int(0.25 * SAMPLE_RATE))  # quarter second of silence
         
         pieces = []
         for sentence in sentences:
-            audio_array = generate_audio(sentence, history_prompt=SPEAKER)
+            audio_array = generate_audio(sentence, history_prompt=self.SPEAKER)
             pieces += [audio_array, silence.copy()]
-                
+             
+        # Convert audio pieces into base64
         buffer = io.BytesIO()
-        sf.write(buffer, np.concatenate(pieces),SAMPLE_RATE, format='WAV')
+        sf.write(buffer, np.concatenate(pieces), SAMPLE_RATE, format='WAV')
         buffer.seek(0)
         base64_audio = base64.b64encode(buffer.read()).decode('utf-8')
-        
-        return {"user_question":user_text,"bot_answer":generated_text,"generated_audio_base64": base64_audio}
+         
+        return {"generated_audio_base64": base64_audio}
+
     def finalize(self):
+        # Finalize resources if needed
         pass
